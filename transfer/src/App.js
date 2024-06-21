0a885e48a11389d14287c430d6323e1b5196fb85
shurik302 <balabuha1988@gmail.com>
2024-06-21 20:55:14 +0200

    конченый коммит

--- transfer/src/App.js
+++ transfer/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from'./pages/Home';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
