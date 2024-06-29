9d0fab6b38998f9b05df5cc7ec0f8a289aa19fe9
shurik302 <balabuha1988@gmail.com>
2024-06-29 18:46:56 +0200

    Additional links комічу, зараз додам додаткові пояснення для користувачів

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,6 +17,27 @@ const Home = () => {
         <div className='PickingPart'>
           <Picking />
         </div>
+        <div className='InfoAndLinks'>
+          <div className='AdditionalLinks'>
+            <a href='' className='FirstAddLink AddLink'>
+              <i class="fa-solid fa-ticket"></i>
+              <span>{t('FirstAddLink')}</span>
+            </a>
+            <a href='' className='SecondAddLink AddLink'>
+              <i class="fa-solid fa-route"></i>
+              <span>{t('SecondAddLink')}</span>
+            </a>
+            <a href='' className='ThirdAddLink AddLink'>
+              <i class="fa-solid fa-question"></i>
+              <span>{t('ThirdAddLink')}</span>
+            </a>
+            <a href='' className='ForthAddLink AddLink'>
+              <i class="fa-solid fa-handshake-angle"></i>
+              <span>{t('FourthAddLink')}</span>
+            </a>
+          </div>
+        </div>
+
       </div>
 
     </div>
