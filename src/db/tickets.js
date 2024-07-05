398495796263aa5a2b015a954c4bd73cce27f74e
shurik302 <balabuha1988@gmail.com>
2024-07-06 01:11:06 +0200

    фікс минулого

--- src/db/tickets.js
+++ src/db/tickets.js
@@ -1,6 +1,6 @@
 export const trips = [
   // Минулі подорожі
-  /* { from: "Kyiv", to: "Warsawa", passengers: "1", date_departure: "01.01.2024", departure: "15:00", duration: "10.5", date_arrival: "02.01.2024", arrival: "01:30", baggage: "no" },
+  { from: "Kyiv", to: "Warsawa", passengers: "1", date_departure: "01.01.2024", departure: "15:00", duration: "10.5", date_arrival: "02.01.2024", arrival: "01:30", baggage: "no" },
   { from: "Warsawa", to: "Kyiv", passengers: "2", date_departure: "10.01.2024", departure: "13:00", duration: "10.5", date_arrival: "10.01.2024", arrival: "23:30", baggage: "yes" },
   { from: "Kyiv", to: "Lviv", passengers: "3", date_departure: "01.02.2024", departure: "09:00", duration: "6", date_arrival: "01.02.2024", arrival: "15:00", baggage: "no" },
   { from: "Lviv", to: "Kyiv", passengers: "4", date_departure: "15.02.2024", departure: "10:00", duration: "6", date_arrival: "15.02.2024", arrival: "16:00", baggage: "yes" },
@@ -14,7 +14,7 @@ export const trips = [
   { from: "Kyiv", to: "Warsawa", passengers: "1", date_departure: "03.07.2024", departure: "18:00", duration: "10.5", date_arrival: "04.07.2024", arrival: "04:30", baggage: "no" },
   { from: "Warsawa", to: "Kyiv", passengers: "2", date_departure: "05.07.2024", departure: "10:00", duration: "10.5", date_arrival: "06.07.2024", arrival: "20:30", baggage: "yes" },
   { from: "Warsawa", to: "Kyiv", passengers: "2", date_departure: "05.07.2024", departure: "16:00", duration: "10.5", date_arrival: "06.07.2024", arrival: "02:30", baggage: "yes" },
-  { from: "Kyiv", to: "Lviv", passengers: "3", date_departure: "04.07.2024", departure: "09:00", duration: "6", date_arrival: "04.07.2024", arrival: "15:00", baggage: "no" }, */
+  { from: "Kyiv", to: "Lviv", passengers: "3", date_departure: "04.07.2024", departure: "09:00", duration: "6", date_arrival: "04.07.2024", arrival: "15:00", baggage: "no" },
 
   // Майбутні подорожі (починаються з 13.07.2024)
   { from: "Kyiv", to: "Warsawa", passengers: "1", date_departure: "13.07.2024", departure: "15:00", duration: "10.5", date_arrival: "14.07.2024", arrival: "01:30", baggage: "no" },
