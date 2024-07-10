b190a2d7b4fb5778d7fc1114eadbba6aec677135
shurik302 <balabuha1988@gmail.com>
2024-07-10 21:05:58 +0200

    Доробив покупку квитка

--- src/pages/BuyTicket.js
+++ src/pages/BuyTicket.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useLocation } from 'react-router-dom';
-import Select from 'react-select';
+import PhoneInput from 'react-phone-input-2';
+import 'react-phone-input-2/lib/style.css';
 import cities from '../db/cities.json';
 import '../stylesheets/BuyTicket.css';
 
@@ -21,7 +22,6 @@ function BuyTicket() {
   const [largeBaggage, setLargeBaggage] = useState(0);
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
-  const [countryCode, setCountryCode] = useState('');
 
   const handleSmallBaggageChange = (amount) => {
     setSmallBaggage(Math.max(0, Math.min(40, smallBaggage + amount)));
@@ -39,7 +39,6 @@ function BuyTicket() {
       largeBaggage,
       email,
       phone,
-      countryCode,
       travel,
       passengers
     });
@@ -52,105 +51,124 @@ function BuyTicket() {
       </div>
       <div className='MainInfoBuyTicketUser'>
         <div className='TicketInfoBuy'>
-          <div className='DataUser'>
-            <input
-              type='text'
-              placeholder={t('FirstName')}
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
-            />
-            <input
-              type='text'
-              placeholder={t('LastName')}
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
-            />
-          </div>
-          <div className='DataBaggage'>
-            <div>{t('HandLuggage')}: {t('Free')}</div>
-            <div>
-              {t('SmallBaggage')}: 5kg, 55x40x20cm
-              <div className='picking-passengers'>
-                <button onClick={() => handleSmallBaggageChange(-1)}>-</button>
-                <input
-                  type='number'
-                  value={smallBaggage}
-                  readOnly
-                />
-                <button onClick={() => handleSmallBaggageChange(1)}>+</button>
-              </div>
-              <div>{t('PriceSmallBaggage')}</div>
+          <div className='UserInfo StageDiv FirstStage'>
+            <div className='NameTitle NameTitleUserInfo'>
+              <span className='Stage'>1</span>
+              <span className='NameTitleBuyTicket NameTitleBuyTicketNameUser'>{t('NameTitleBuyTicketNameUser')}</span>
             </div>
-            <div>
-              {t('LargeBaggage')}: 20kg, 80x50x30cm
-              <div className='picking-passengers'>
-                <button onClick={() => handleLargeBaggageChange(-1)}>-</button>
-                <input
-                  type='number'
-                  value={largeBaggage}
-                  readOnly
-                />
-                <button onClick={() => handleLargeBaggageChange(1)}>+</button>
-              </div>
-              <div>{t('PriceLargeBaggage')}</div>
-            </div>
-          </div>
-          <div className='DataContactUser'>
-            <input
-              type='email'
-              placeholder={t('Email')}
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <div className='phone-input'>
-              <Select
-                className="country-code-select"
-                value={{ value: countryCode, label: countryCode }}
-                onChange={(selectedOption) => setCountryCode(selectedOption.value)}
-                options={[
-                  { value: '+380', label: '+380' },
-                  { value: '+48', label: '+48' },
-                  { value: '+1', label: '+1' }
-                ]}
+            <div className='MainInputs MainInputsNameUser'>
+              <input
+                type='text'
+                placeholder={t('FirstName')}
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
               />
               <input
-                type='tel'
-                placeholder={t('Phone')}
-                value={phone}
-                onChange={(e) => setPhone(e.target.value)}
+                type='text'
+                placeholder={t('LastName')}
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
               />
             </div>
+          </div>
+
+          <div className='DataBaggage StageDiv SecondStage'>
+            <div className='NameTitle NameTitleUserInfo'>
+              <span className='Stage'>2</span>
+              <span className='NameTitleBuyTicket NameTitleBuyTicketDataBaggage'>{t('NameTitleBuyTicketDataBaggage')}</span>
+            </div>
+            <div className='MainInputs MainInputsDataBaggage'>
+              <div className='HandLuggage BaggageDiv'><span>{t('HandLuggage')}:</span><span>{t('Free')}</span></div>
+              <div className='GorisontalLine'></div>
+              <div className='SmallBaggage BaggageDiv'>
+                <div className='InfoBaggageType'>
+                  <span>{t('SmallBaggage')}:</span><span className='AddInfoBaggage'> 5kg, 55x40x20cm</span>
+                </div>
+                <div className='RightPartBaggage'>
+                  <div className='picking-baggage'>
+                    <button onClick={() => handleSmallBaggageChange(-1)}>-</button>
+                    <input
+                      type='number'
+                      value={smallBaggage}
+                      readOnly
+                    />
+                    <button onClick={() => handleSmallBaggageChange(1)}>+</button>
+                  </div>
+                  <div><span>{t('120грн'/* PriceSmallBaggage */)}</span></div>
+                </div>
+              </div>
+              <div className='LargeBaggage BaggageDiv'>
+                <div className='InfoBaggageType'>
+                  <span>{t('LargeBaggage')}:</span><span className='AddInfoBaggage'> 20kg, 80x50x30cm</span>
+                </div>
+
+                <div className='RightPartBaggage'>
+                  <div className='picking-baggage'>
+                    <button onClick={() => handleLargeBaggageChange(-1)}>-</button>
+                    <input
+                      type='number'
+                      value={largeBaggage}
+                      readOnly
+                    />
+                    <button onClick={() => handleLargeBaggageChange(1)}>+</button>
+                  </div>
+                  <div><span>{t('130грн'/* PriceLargeBaggage */)}</span></div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className='DataContactUser StageDiv ThirdStage'>
+            <div className='NameTitle NameTitleDataContactUser'>
+              <span className='Stage'>3</span>
+              <span className='NameTitleBuyTicket NameTitleBuyTicketDataDataContactUser'>{t('NameTitleBuyTicketDataDataContactUser')}</span>
+            </div>
+            <div className='DataContactUserMain'>
+              <div className='EnterEmailBT'>
+                <input
+                  type='email'
+                  placeholder={t('Email')}
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className='phone-input'>
+                <PhoneInput
+                  country={'us'}
+                  value={phone}
+                  onChange={setPhone}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='AddInfoTicketBuy'>
+          <div className='ProgressTicketBuyVertical'>
+            <div className='RouteSymbol'>
+              <div className='Line'></div>
+              <div className='Circle top'>
+                <div className='InnerCircle'>
+                  <div className='SmallCircle'></div>
+                </div>
+              </div>
+              <div className='Circle bottom'>
+                <div className='InnerCircle'>
+                  <div className='SmallCircle'></div>
+                </div>
+              </div>
+            </div>
+            <div className='FromToTicketTicketBuyVertical'>
+              <div className='FromTicket'><span>{getCityNameById(travel.from, language)}</span><span>{travel.departure}</span></div>
+              <div className='ToTicket'><span>{getCityNameById(travel.to, language)}</span><span>{travel.arrival}</span></div>
+            </div>
+          </div>
+          <div className='AddInfoTicketBT'>
+            <div className='DurationTicket'><span>{t('DurationTicket')}</span><span>{travel.duration}{t('hours')}</span></div>
+
           </div>
           <div className='ButtonConfirmBuyTicket'>
             <button onClick={handleBuyTicket}>{t('ConfirmPurchase')}</button>
-          </div>
-        </div>
-        <div className='AddInfoTicketBuy'>
-          <div className='FromToTicket'>
-            <div className='FromTicket'><span>{getCityNameById(travel.from, language)}</span></div>
-            <div className='ToTicket'><span>{getCityNameById(travel.to, language)}</span></div>
-          </div>
-          <div className='ProgressSymbol'>
-            <div className='Line'></div>
-            <div className='Circle left'>
-              <div className='InnerCircle'>
-                <div className='SmallCircle'></div>
-              </div>
-            </div>
-            <div className='Circle right'>
-              <div className='InnerCircle'>
-                <div className='SmallCircle'></div>
-              </div>
-            </div>
-          </div>
-          <div className='TimeTicket'>
-            <div className='DepartureTicket'><span>{travel.departure}</span></div>
-            <div className='ArrivalTicket'><span>{travel.arrival}</span></div>
-          </div>
-          <div className='AddInfoTicket'>
-            <div className='DurationTicket'><span>{travel.duration} {t('hours')}</span></div>
-            <div className='PassengersTicket'><span>{t('Passengers')}: {travel.passengers}</span></div>
-            <div className='BaggageTicket'><span>{t('Baggage')}: {travel.baggage}</span></div>
           </div>
         </div>
       </div>
